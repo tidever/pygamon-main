da8c6767e81e9fb444ff214c5457a47909820ac6
tidever <thyllianvetault44@gmail.com>
2024-05-23 01:45:45 +0200

    Add files via upload
    logiciel requis: Visual Studio Code ou Pycharm et python d'installer sur votre ordinateur pour installer pygame, pytmx et pyscroll dans le cmd de votre ordinateur:
    pip install pygame
    pip install pytmx
    pip install pyscroll

---
+++ Pygamon-main/game.py
@@ -0,0 +1,69 @@
+import pygame
+from pygame.constants import K_DOWN
+import pytmx
+import pyscroll
+from dialog import DialogBox
+from map import MapManager
+from player import Player
+
+
+
+class Game:
+    def __init__(self) -> None:
+
+        # creer la fenetre du jeux
+        self.screen = pygame.display.set_mode((800, 600))
+        pygame.display.set_caption("Pygamon - Aventure")
+
+        
+        # genere un joureur
+        self.player = Player()
+        
+        self.map_manager = MapManager(self.screen, self.player)
+    
+        self.dialog_box = DialogBox()
+        
+    def handle_input(self):
+        pressed = pygame.key.get_pressed()
+        
+        if pressed[pygame.K_UP]:
+            self.player.move_up()
+        elif pressed[pygame.K_DOWN]:
+            self.player.move_down()
+        elif  pressed[pygame.K_LEFT]:
+            self.player.move_left()
+        elif  pressed[pygame.K_RIGHT]:
+            self.player.move_right()
+        
+    
+        
+    def update(self):
+        self.map_manager.update()
+        
+                    
+            
+    def run(self):
+
+        clock = pygame.time.Clock()
+        
+        # boucle du jeux
+        running = True 
+
+        while running: 
+            
+            self.player.save_location()
+            self.handle_input()
+            self.update()
+            self.map_manager.draw()
+            self.dialog_box.render(self.screen)
+            pygame.display.flip()
+            
+            for event in pygame.event.get() :
+                if event.type == pygame.QUIT:
+                    running = False 
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_SPACE:
+                        self.map_manager.check_npc_collisions(self.dialog_box)
+                    
+        clock.tick(40)
+        pygame.quit()
---
+++ Pygamon-main/main.py
@@ -0,0 +1,9 @@
+from typing import Mapping
+import pygame
+from game import Game
+
+
+if __name__ == '__main__':
+    pygame.init()
+    game = Game()
+    game.run()
